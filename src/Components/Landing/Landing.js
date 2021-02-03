6888c66c47c0f3ea88f6127846f138b2fa31c9db
agiannell <agiannell@gmail.com>
2021-02-03 10:09:34 -0700

    styling in sass

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -7,8 +7,17 @@ function Landing() {
         <section>
             <LandingHeader />
             <section className='landing-main'>
-                <p>you need unlocked</p>
-                <Link to='/signin'><button>Sign In</button></Link>
+                <section className='landing-content'>
+                    <section className='landing-info'>
+                        <h1>Take Control of Your Finances</h1>
+                        <p>With unlocked, you can take back control of your money. It's time you make your money work for you, not the other way around. Simply sign in, get your monthly plan set up, and start telling your money what to do.</p>
+                        <Link to='/signin'><button>Sign In</button></Link>
+                    </section>
+                    <section className='budget-preview'>
+                        <h2>Your Transactions</h2>
+                        <div className='transaction'></div>
+                    </section>
+                </section>
             </section>
         </section>
     );
